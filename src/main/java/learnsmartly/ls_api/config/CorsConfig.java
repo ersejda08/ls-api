ccdf6fa43ae5884ff198ada9b01939c5ad16f191
Ersejda <ersejdademirxhiu@gmail.com>
2026-01-22 22:18:34 +0100

    fixed the issue with cors config

--- src/main/java/learnsmartly/ls_api/config/CorsConfig.java
+++ src/main/java/learnsmartly/ls_api/config/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                    .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedOriginPatterns("*")
+                    .allowedMethods("*")
                     .allowedHeaders("*")
                     .allowCredentials(true)
                     .maxAge(3600);
